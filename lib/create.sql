da784fa5a2339b27bbe550983d7e5c7942583fad
Jordan Johnson <jkj86@hotmail.com>
2017-09-02 00:54:41 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE projects(
+  title TEXT,
+  category TEXT,
+  funding_goal REAL,
+  startdate datetime,
+  enddate datetime
+);
+CREATE TABLE users(
+  
+);
+CREATE TABLE pledges(
+  
+);
